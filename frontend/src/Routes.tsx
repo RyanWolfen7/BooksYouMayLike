7728954edba168e2b8cca55b99f8b94a29c3316c
Ryan Clark <wolfen7@pop-os.localdomain>
2020-02-18 12:01:06 +0000

    Setup frontend skeleton

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+
+export default () => {
+  return (
+    <Switch>
+      <Route
+        exact
+        path={'/'}
+      />
+    </Switch>
+  )
+}
