e22cec84e90428921ac87253256ba0becd413f44
Ryan Clark <wolfen7@pop-os.localdomain>
2020-03-12 09:46:14 +0000

    added link to add data to add book for dynamic dispaly

--- frontend/src/components/AddBook/AddBook.js
+++ frontend/src/components/AddBook/AddBook.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MainViewStyles } from '../../styles/MainViewStyles'
 import { Grid } from '@material-ui/core'
+import SaveBookForm from '../SaveBookForm/SaveBookForm'
+import NavBar from '../NavBar/NavBar'
 
 const AddBook = props => {
     const classes = MainViewStyles()
+    const [ book, setBook ] = useState({
+        name: '',
+        writer: '',
+        date_published: '',
+        cover: '',
+        summary: ''
+    })
 
+    const updateBookDetails = bookForm => {
+        setBook(bookForm)
+    }
 
     return (<> 
+        <NavBar/>
         <Grid container className={classes.root} spacing={2}>
             <Grid item xs={6} className={classes.details}>
+                <SaveBookForm book={book} updateBookDetails={updateBookDetails}/>
             </Grid>
             <Grid item xs={6} className={classes.details}>
             </Grid>
--- frontend/src/components/SaveBookForm/SaveBookForm.js
+++ frontend/src/components/SaveBookForm/SaveBookForm.js
@@ -7,9 +7,11 @@ import actions from '../../actions'
 
 const SaveBookForm = props => {
     const classes = SaveBookFormStyles()
+    const { updateBookDetails } = props
     const [book, setBook] = useState(props.book)
     const dispatch = useDispatch()
     const date = new Date(book.date_published * 1000).toISOString().split('T')[0]
+    if(updateBookDetails) { updateBookDetails(book)}
 
     const onChange = event => {
         let value = event.target.value
@@ -23,64 +25,63 @@ const SaveBookForm = props => {
         dispatch(actions.booksList.updateBook(book))
         window.location.reload(false)
     }
-
     return (
         <Container className={classes.container}>
             <form className={classes.form} autoComplete='off'>
-                    <TextField
-                        id="name"
-                        label="Name"
-                        multiline
-                        rowsMax="4"
-                        value={book.name}
-                        width={1}
-                        onChange={event => onChange(event)}
-                    />
-                    <TextField
-                        id="writer"
-                        label="Writer"
-                        value={book.writer}
-                        width={1}
-                        onChange={event => onChange(event)}
-                    />
-                    <TextField
-                        id="date_published"
-                        label="Date Published"
-                        type='date'
-                        variant="outlined"
-                        value={date}
-                        width={1}
-                        onChange={event => onChange(event)}
-                    />
-                    <TextField
-                        id="cover"
-                        label="Cover URL"
-                        value={book.cover}
-                        width={1}
-                        onChange={event => onChange(event)}
-                    />
-                    <TextField
-                        id="summary"
-                        label="Summary"
-                        multiline
-                        rowsMax="4"
-                        value={book.summary}
-                        width={1}
-                        onChange={event => onChange(event)}
-                    />
-                    <Grid container justify='center'>
-                        <Button
-                            varient="contained"
-                            className={classes.save}
-                            startIcon={<SaveIcon/>}
-                            onClick={() => handleSave()}
-                        > Save </Button>
-                        <Button
-                            varient="contained"
-                            className={classes.delete}
-                            startIcon={<DeleteIcon/>}
-                        > Delete </Button>
-                    </Grid>
+                <TextField
+                    id="name"
+                    label="Name"
+                    multiline
+                    rowsMax="4"
+                    value={book.name}
+                    width={1}
+                    onChange={event => onChange(event)}
+                />
+                <TextField
+                    id="writer"
+                    label="Writer"
+                    value={book.writer}
+                    width={1}
+                    onChange={event => onChange(event)}
+                />
+                <TextField
+                    id="date_published"
+                    label="Date Published"
+                    type='date'
+                    variant="outlined"
+                    value={date}
+                    width={1}
+                    onChange={event => onChange(event)}
+                />
+                <TextField
+                    id="cover"
+                    label="Cover URL"
+                    value={book.cover}
+                    width={1}
+                    onChange={event => onChange(event)}
+                />
+                <TextField
+                    id="summary"
+                    label="Summary"
+                    multiline
+                    rowsMax="4"
+                    value={book.summary}
+                    width={1}
+                    onChange={event => onChange(event)}
+                />
+                <Grid container justify='center'>
+                    <Button
+                        varient="contained"
+                        className={classes.save}
+                        startIcon={<SaveIcon/>}
+                        onClick={() => handleSave()}
+                    > Save </Button>
+                    <Button
+                        varient="contained"
+                        className={classes.delete}
+                        startIcon={<DeleteIcon/>}
+                    > Delete </Button>
+                </Grid>
             </form>
         </Container>
     
